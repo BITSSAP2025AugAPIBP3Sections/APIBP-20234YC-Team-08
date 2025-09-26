e13493fbb8d7ade1ba38384f5254acc1a3121fd2
pranavpa8788 <pranavpa8788@gmail.com>
2025-09-26 19:28:28 +0530

    Minor fixes

--- src/app.py
+++ src/app.py
@@ -35,6 +35,3 @@ if st.button("Predict") and canvas.image_data is not None:
     x = x[np.newaxis, ..., np.newaxis]
     pred = model.predict(x)
     st.write(f"Predicted digit: {int(np.argmax(pred[0]))}")
-
-port = int(os.environ.get("PORT", 8501))
-st.run_server("0.0.0.0", port=port)
